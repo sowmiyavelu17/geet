23c8f87a73ffefd8d673995d1c7212bee4daa17f
sowmiyavelu17 <53555335+sowmiyavelu17@users.noreply.github.com>
2019-08-09 14:43:48 +0530

    Create int.py

---
+++ int.py
@@ -0,0 +1,11 @@
+str_input1=input("Enter First String? ")
+ 
+str_input2=input("Enter Second String? ")
+ 
+if str_input1 == str_input2:
+ 
+    print ("First and second strings are same!")
+ 
+else:
+ 
+    print ("You entered different strings!")
