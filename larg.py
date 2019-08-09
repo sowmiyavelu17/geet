6ea1bf00a5ab989266a220c1b22ba55ced813c3c
sowmiyavelu17 <53555335+sowmiyavelu17@users.noreply.github.com>
2019-08-09 14:17:42 +0530

    Create larg.py

---
+++ larg.py
@@ -0,0 +1,15 @@
+def maximum(a, b, c): 
+ 
+    if (a >= b) and (a >= b): 
+        largest=a 
+ 
+    elif (b >= a) and (b >= a): 
+        largest=b 
+    else: 
+        largest=c 
+ 
+    return largest 
+a=10
+b=14
+c=12
+print(maximum(a, b, c)) 
