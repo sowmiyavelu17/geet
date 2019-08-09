164fef8f76b197658fbff0ab7f9e14cda7e63d36
sowmiyavelu17 <53555335+sowmiyavelu17@users.noreply.github.com>
2019-08-09 15:18:01 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,16 @@
+using namespace std;
+bool isPowerOfTwo(int n) 
+{ 
+   if(n==0) 
+   return false; 
+  
+   return (ceil(log2(n)==floor(log2(n))); 
+} 
+ 
+int main() 
+{ 
+    isPowerOfTwo(31)? cout<<"Yes"<<endl: cout<<"No"<<endl; 
+    isPowerOfTwo(64)? cout<<"Yes"<<endl: cout<<"No"<<endl; 
+  
+    return 0; 
+} 
